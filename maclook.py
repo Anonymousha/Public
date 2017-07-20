d31afd7592e1b58ed7d06d6c93e29cb58c3ae52f
Eitenne <finnurs@protonmail.ch>
2017-07-20 19:29:26 +0200

    Create maclook.py

---
+++ maclook.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+#Author: Eitenne
+
+import urllib
+import sys
+
+if len(sys.argv) == 2:
+	mac = sys.argv[1]
+	link = "http://api.mylnikov.org/geolocation/wifi?v=1.1&data=wpa2,wpa,wep,open&bssid=%s" % (mac)
+	data = urllib.urlopen(link)
+	content = data.read()
+        print content
+else:
+	print "[!] Usage: ./maclook 00:00:00:00"
