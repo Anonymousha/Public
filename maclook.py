d730dfedbe2f4865018c5e854b37ee4b1c0253f3
Eitenne <finnurs@protonmail.ch>
2017-07-27 15:50:37 +0200

    Update maclook.py

--- maclook.py
+++ maclook.py
@@ -4,12 +4,27 @@
 
 import urllib
 import sys
+import json
+from geopy.geocoders import Nominatim
 
 if len(sys.argv) == 2:
 	mac = sys.argv[1]
+	geolocator = Nominatim()
 	link = "http://api.mylnikov.org/geolocation/wifi?v=1.1&data=wpa2,wpa,wep,open&bssid=%s" % (mac)
 	data = urllib.urlopen(link)
 	content = data.read()
-        print content
+	obj = json.loads(content)
+	lat = obj['data']['lat']
+	long = obj['data']['lon']
+        print "Lat: " + str(lat)
+        print "Long" + str(long)
+        place = str(lat) + "," + str(long)
+        location = geolocator.reverse(place)
+        print "Location: " + str(location)
 else:
-	print "[!] Usage: ./maclook 00:00:00:00"
+	print """Maclook v3.0( Eitenne )
+Usage: maclook [BSSID]
+     
+EXAMPLES:
+        python maclook.py 00:00:00:00
+"""
